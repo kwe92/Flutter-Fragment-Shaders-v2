7306328ce36730665d69dad91fcdc44abbdd128f
Kwe <Kweayon@gmail.com>
2024-10-30 13:47:09 -0500

    task: finished lesson and added comments

--- lib/styles.dart
+++ lib/styles.dart
@@ -4,14 +4,14 @@
 
 import 'package:flutter/material.dart';
 
+//!! I like how TextStyle's are consolidated into a Namespace
 class TextStyles {
   static const _font1 = TextStyle(fontFamily: 'Exo', color: Colors.white);
 
-  static TextStyle get h1 => _font1.copyWith(
-      fontSize: 75, letterSpacing: 35, fontWeight: FontWeight.w700);
+  //!! Static Derivations (Comuted Fields)
+  static TextStyle get h1 => _font1.copyWith(fontSize: 75, letterSpacing: 35, fontWeight: FontWeight.w700);
   static TextStyle get h2 => h1.copyWith(fontSize: 40, letterSpacing: 0);
-  static TextStyle get h3 =>
-      h1.copyWith(fontSize: 24, letterSpacing: 20, fontWeight: FontWeight.w400);
+  static TextStyle get h3 => h1.copyWith(fontSize: 24, letterSpacing: 20, fontWeight: FontWeight.w400);
   static TextStyle get body => _font1.copyWith(fontSize: 16);
   static TextStyle get btn => _font1.copyWith(
         fontSize: 16,
