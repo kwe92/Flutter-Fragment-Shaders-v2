eeb143b5216133fe86c460c6192f8a1dff77a143
Kwe <Kweayon@gmail.com>
2024-10-30 13:47:19 -0500

    task: finished lesson and added comments

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,30 @@ import 'dart:io' show Platform;
 
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:provider/provider.dart';
 import 'package:window_size/window_size.dart';
 
-import 'styles.dart';
+import 'assets.dart';
+import 'titleScreen/title_screen.dart';
 
 void main() {
   if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
     WidgetsFlutterBinding.ensureInitialized();
     setWindowMinSize(const Size(800, 500));
   }
-  runApp(const NextGenApp());
+  Animate.restartOnHotReload = true;
+
+  runApp(
+    //?? Working with FutureProvider
+    FutureProvider<FragmentPrograms?>(
+      create: (context) => loadFragmentPrograms(),
+      initialData: null,
+      builder: (context, _) {
+        return const NextGenApp();
+      },
+    ),
+  );
 }
 
 class NextGenApp extends StatelessWidget {
@@ -24,16 +38,10 @@ class NextGenApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // ?? theme mode and dark theme combination?
       themeMode: ThemeMode.dark,
       darkTheme: ThemeData(brightness: Brightness.dark),
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Insert Next-Generation UI Here...',
-            style: TextStyles.h2,
-          ),
-        ),
-      ),
+      home: const TitleScreen(),
     );
   }
 }
